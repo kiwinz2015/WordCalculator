d3dc2c1f8acec07fb521b22a1c37a7cda8bcb5e4
kiwinz2015 <51891425+kiwinz2015@users.noreply.github.com>
2020-02-11 18:16:18 +1300

    binging project to repo

---
+++ WordCalculator/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = "";
+            /*
+            //### Step 1
+            Console.WriteLine("\n\nStep #1/12 Create a simple string calculator with a method that takes a string and returns a number. :)");
+            Console.WriteLine($"Type a word: ");
+            word = Console.ReadLine();
+            Console.WriteLine($"The word lenght is: <<< {WordCalc.myWordLength(word)} >>>"); //{word}
+
+            //### Step 2
+            Console.WriteLine($"\n\nStep #2/12 A single number returns that number \nType a number: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Your number is:{WordCalc.ReturnMyNumber(a)}");
+
+            //### Step 3
+            Console.WriteLine($"\n\nStep #3/12 Two numbers return the sum of the numbers: ");
+            Console.WriteLine($"Enter operator A:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Enter operator B:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"A + B = {WordCalc.SimpleSum(a,b)}");
+
+            //### Step 4
+            Console.WriteLine($"\n\nStep #4/12 Any amount of numbers returns the sum of those numbers: ");
+            Console.WriteLine($"All numbers entered sums = {WordCalc.MultipleSum()}");
+
+    */
+            //### Step 5
+            Console.WriteLine($"\n\nStep #5/12 New line breaks and commas should be interchangeable between numbers. : ");
+            Console.WriteLine($"Enter a few numbers including line breaks, eg: 1,2\n3 ");
+            word = Console.ReadLine();
+            Console.WriteLine($"The sum equals to: {WordCalc.Interchangeable(word)}");
+
+            //================================ 
+            //Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+
+    }
+
+    class WordCalc
+    {
+        //### Step 1 
+        public static int myWordLength(string word)
+        {
+            var res = word.Count();
+            return res;
+        }
+
+        //### Step 2
+        public static double ReturnMyNumber(double number) => number;
+
+        //## Step 3 
+        public static double SimpleSum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double MultipleSum()
+        {
+            double mynumber;
+            double[] numbs = new double[100];
+            int i = 0;
+            Console.WriteLine($"Enter numbers OR Press 0 to Exit:");
+
+            do
+            {
+                mynumber = Convert.ToDouble(Console.ReadLine());
+
+                numbs[i] = mynumber;
+
+                i++;
+            }
+            while (mynumber != 0);
+
+            return numbs.Sum();
+        }
+
+        internal static object ReturnMyNumber(bool v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int Interchangeable(string a)
+        {
+            string b = string.Empty;
+            int val = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (Char.IsDigit(a[i]))
+                {
+                    b = a[i].ToString();
+                    val = val + int.Parse(b);
+                }
+            }
+
+            return val;
+        }
+
+        public static int nuevo() 
+        {
+            return 1;
+        }
+    }
+}
