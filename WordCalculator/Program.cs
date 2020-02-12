f71234c338b1f5433e7fb4a3292b4acb9ce3be39
Santiago Almiron <Santiago.Almiron@serko.com>
2020-02-12 14:55:42 +1300

    Step 8

--- WordCalculator/Program.cs
+++ WordCalculator/Program.cs
@@ -34,14 +34,19 @@ namespace String_Calculator
             //### Step 4
             Console.WriteLine($"\n\nStep #4/12 Any amount of numbers returns the sum of those numbers: ");
             Console.WriteLine($"All numbers entered sums = {WordCalc.MultipleSum()}");
-
-    */
-            //### Step 5
+    
+            //### Step 5 & 6
             Console.WriteLine($"\n\nStep #5/12 New line breaks and commas should be interchangeable between numbers. : ");
             Console.WriteLine($"Enter a few numbers including line breaks, eg: 1,2\n3 ");
             word = Console.ReadLine();
             Console.WriteLine($"The sum equals to: {WordCalc.Interchangeable(word)}");
 
+            */
+
+            //### Step 7 & 8
+            Console.WriteLine($"\n\nStep #7/12 Any amount of POSITIVE numbers returns the sum of those numbers: ");
+            Console.WriteLine($"All numbers entered sums = {WordCalc.MultiplePositiveSum()}");
+            
             //================================ 
             //Console.Write("\nPress any key to exit...");
             Console.ReadKey(true);
@@ -108,12 +113,30 @@ namespace String_Calculator
 
             return val;
         }
-
-        public static int nuevo() 
+        
+        public static double MultiplePositiveSum()
         {
-            //test from the office >>> push this change to origin marter
-            return 1;
+            double mynumber;
+            double[] numbs = new double[100];
+            int i = 0;
+            Console.WriteLine($"Enter numbers OR Press 0 to Exit:");
 
+            do
+            {
+                mynumber = Convert.ToDouble(Console.ReadLine());
+                if (mynumber > 0 &&  mynumber < 1000)
+                {
+                    numbs[i] = mynumber;
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine($"Negative numbers OR Greater than 999 not allowed. You've entered:{mynumber}");
+                }
+            }
+            while (mynumber != 0);
+
+            return numbs.Sum();
         }
     }
 }
