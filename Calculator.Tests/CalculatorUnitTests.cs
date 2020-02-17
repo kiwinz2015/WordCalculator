f33b773b15c73eaf0ab49b10e4e6622c3fc09119
kiwinz2015 <51891425+kiwinz2015@users.noreply.github.com>
2020-02-17 19:36:01 +1300

    Add XUnit Project and CalculatorUnitTests Class including first unit test for a simple sum. Rename Programs Class to WordCalculations Class

---
+++ Calculator.Tests/CalculatorUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using String_Calculator;
+using Xunit;
+
+namespace Calculator.Tests
+{
+
+    public class CalculatorUnitTests
+    {
+        [Fact]
+        public void Add_TwoNumbers()
+        {
+            // Arrange
+            double expected = 5;
+
+            // Act
+            double actual_result = WordCalc.SimpleSum(2, 3);
+
+            // Assert
+            Assert.Equal(expected, actual_result);
+
+        }
+
+    }
+}
--- WordCalculator/WordCalculations.cs
+++ WordCalculator/WordCalculations.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xunit;
 
 namespace String_Calculator
 {
@@ -54,7 +53,7 @@ namespace String_Calculator
 
     }
 
-    class WordCalc
+    public static class WordCalc
     {
         //### Step 1 
         public static int myWordLength(string word)
